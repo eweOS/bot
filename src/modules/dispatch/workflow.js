323043be3df343168b18c6ceb8c9667ff5eca067
Yukari Chiba <i@0x7f.cc>
2024-08-03 02:17:33 +0800

    ignore pr_closed

--- src/modules/dispatch/workflow.js
+++ src/modules/dispatch/workflow.js
@@ -5,11 +5,18 @@ const mod = {
   func: mod_fn,
 };
 
+const ignored_workflows = ["pr_closed.yml"];
+
 async function mod_fn(payload) {
   if (payload.repository.full_name !== "eweOS/workflow") return;
-  if (payload.action == "completed") {
-    await sendWorkflowStatus(payload);
-  }
+  if (payload.action != "completed") return;
+
+  let workflow = payload.workflow_run;
+  let workflowid = workflow.path.split("/").at(-1);
+
+  if (ignored_workflows.includes(workflowid)) return;
+
+  await sendWorkflowStatus(payload);
 }
 
 export default mod;
